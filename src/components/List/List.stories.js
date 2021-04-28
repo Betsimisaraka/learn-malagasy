c3268183f4e14033adabeaa945aadd1c6131bf08
Anita <anita.ras@onja.org>
2021-04-28 13:52:43 +0300

    Added a list component

---
+++ src/components/List/List.stories.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import {View} from 'react-native';
+import {storiesOf} from '@storybook/react-native';
+
+// const categoriesData = require('./categories.json');
+const categoriesData = require('../../data/categories.json');
+
+import List from './List';
+
+function GetList() {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    function getCategory() {
+      const categories = categoriesData.categories;
+      setCategory(categories);
+    }
+    getCategory();
+  }, []);
+
+  return (
+    <List
+      item={category}
+      onPress={() => alert('Learn more words here')}
+      buttonText="Learn"
+    />
+  );
+}
+
+storiesOf('List', module)
+  .addDecorator(story => <View style={{margin: 20}}>{story()}</View>)
+  .add('learn', () => <GetList />);
